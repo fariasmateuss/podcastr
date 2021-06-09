39addd948354962e4f769a2e28941d39c3592fec
Mateus V. Farias <mateus_vinicius15@outlook.com>
2021-06-09 16:13:00 -0400

    feat: add formatting for current date

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,21 @@
+/* eslint-disable import/no-duplicates */
+import format from 'date-fns/format';
+import enUS from 'date-fns/locale/en-US';
+
 import styles from '@/styles/components/Header.module.scss';
 
 export function Header() {
+  const currentDate = format(new Date(), `d MMMM`, {
+    locale: enUS,
+  });
+
   return (
     <header className={styles.headerContainer}>
       <img src="/logo.svg" alt="Podcastr" />
 
       <p>The best for you to hear, always</p>
 
-      <span>Jun 8</span>
+      <span>{currentDate}</span>
     </header>
   );
 }
