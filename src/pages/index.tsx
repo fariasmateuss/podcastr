afec0869440d9e6022e6f39106225f1cbbdc8070
Mateus V. Farias <mateus_vinicius15@outlook.com>
2021-06-12 17:02:37 -0400

    feat: fetch data using getServerSideProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,3 +7,16 @@ export default function Home() {
     </div>
   );
 }
+
+export async function getServerSideProps() {
+  const response = await fetch(`http://localhost:3333/episodes`);
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+
+    revalidate: 60 * 60 * 8,
+  };
+}
