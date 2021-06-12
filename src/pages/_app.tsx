7ea718283d2b41a70d22141746db0f93cde96744
Mateus V. Farias <mateus_vinicius15@outlook.com>
2021-06-12 00:16:09 -0400

    feat: add player component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,20 @@
 import { AppProps } from 'next/app';
 import { Header } from '@/components/Header';
+import { Player } from '@/components/Player';
+
+import styles from '../styles/pages/app.module.scss';
 
 import '@/styles/global.scss';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <main>
-      <Header />
-      <Component {...pageProps} />
-    </main>
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+
+      <Player />
+    </div>
   );
 }
